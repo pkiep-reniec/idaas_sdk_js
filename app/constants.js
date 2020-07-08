5423a1fb3608706e7b0cceca9894d9b721a49f37
Miguel Pazo <miguel.ps19@gmail.com>
2020-07-07 22:42:53 -0500

    update acr

--- app/constants.js
+++ app/constants.js
@@ -3,22 +3,15 @@
  */
 
 var ReniecIdaasConst = {
-    ACR_ONE_FACTOR: 'one_factor',
-    ACR_TWO_FACTOR: 'two_factor',
     ACR_PKI_DNIE: 'pki_dnie',
     ACR_PKI_TOKEN: 'pki_token',
-    ACR_ONLY_PASSWORD: 'only_password',
-    ACR_ONLY_QUESTIONS: 'only_questions',
     ACR_PKI_DNIE_MOBILE: 'pki_dnie_mobile',
-    ACR_FINGERPRINT_MOBILE: 'fingerprint_mobile',
     ACR_PKI_DNIE_LEGACY: 'pki_dnie_legacy',
     ACR_PKI_TOKEN_LEGACY: 'pki_token_legacy',
     PROMPT_NONE: 'none',
     PROMPT_LOGIN: 'login',
     PROMPT_CONSENT: 'consent',
     SCOPE_PROFILE: 'profile',
-    SCOPE_EMAIL: 'email',
-    SCOPE_PHONE: 'phone',
     SCOPE_OFFLINE_ACCESS: 'offline_access',
     RESPONSE_TYPE_CODE: 'code',
     RESPONSE_TYPE_ID_TOKEN: 'id_token',
